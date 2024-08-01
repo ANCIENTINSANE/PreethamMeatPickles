d04f2bee86e888cb93672a036fdc7b4587bdfd80
ANCIENTINSANE <surendram.dev@gmail.com>
2024-08-01 11:45:13 +0530

    TailWind Init {Status: Ready for Development}

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <h1 className="text-6xl font-extrabold underline text-[#278a69cc]">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      '3xs': '320px',
+      // => @media (min-width: 640px) { ... }
+
+      '2xs': '375px',
+      // => @media (min-width: 640px) { ... }
+
+      'xs': '425px',
+      // => @media (min-width: 640px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      '2md': '845px',
+      // => @media (min-width: 845px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1536px) { ... }
+  },
+  },
+  plugins: [],
+}
